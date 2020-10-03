da9d513da719929e1428adb60f9fa4c6774ff21e
Julie Pinchak <jpinchak@gmail.com>
2020-10-03 12:22:31 -0700

    set up project file structure and served static files

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({ origin: ["http://localhost:8080", "http://localhost:3000"] })
+);
+
+app.use(express.static('public'));
+
+module.exports = app.listen(PORT, () => {
+  console.log("Aql hears you loud and clear on port 3000");
+});
