6914216fc103e8d011830c142b69cefc8ce769be
Case Simmons <casesimmons@pm.me>
2020-10-03 15:42:27 -0700

    (create) SCSS structure, NavBar component, (update) webpack; dev-server proxy working. Co-authored-by: Julie Pinchak <jpinchak@gmail.com>

--- server/server.js
+++ server/server.js
@@ -5,12 +5,10 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({ origin: ["http://localhost:8080", "http://localhost:3000"] })
-);
+app.use(cors({ origin: ['http://localhost:8080', 'http://localhost:3000'] }));
 
 app.use(express.static('public'));
 
 module.exports = app.listen(PORT, () => {
-  console.log("Aql hears you loud and clear on port 3000");
+  console.log('Aql hears you loud and clear on port 3000');
 });
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+// import * as d3 from 'd3';
+import Navbar from './components/Navbar.jsx';
+import '../public/scss/application.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
