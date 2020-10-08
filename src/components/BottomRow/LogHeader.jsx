824629f4b68ac5c25c5196e77f95eebdf3162fe6
Case Simmons <casesimmons@pm.me>
2020-10-07 21:38:48 -0700

    (create) LogHeader component
    Co-Authored-by: Michael O'Halloran <45404541+LordRegis22@users.noreply.github.com>

---
+++ src/components/BottomRow/LogHeader.jsx
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+/**
+ * Component that renders a Log Entry
+ */
+
+function LogHeader() {
+  return <div id="log"></div>;
+}
+
+export default LogHeader;
