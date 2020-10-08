a197b99bc2e17fb09e51711415d8bfddfe2ba485
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-07 19:47:16 -0700

    Change format of date on x-axis and rotate the labels.
    co-authored-by: Julie Pinchak <jpinchak@gmail.com>

--- src/components/TopRow/LineChart.jsx
+++ src/components/TopRow/LineChart.jsx
@@ -8,6 +8,8 @@ import {
   curveCardinal,
   axisBottom,
   axisLeft,
+  timeFormat,
+  tickFormat,
   zoom,
   zoomTransform,
 } from 'd3';
@@ -18,12 +20,13 @@ import useResizeObserver from '../useResizeObserver';
  */
 
 function LineChart(props) {
-  const avgLatency = props.mutationData.map(elt => ({avgL: elt.avgLatency, mutDate: elt.dateTime})); 
+  const avgLatency = props.mutationData.map(elt => ({avgL: elt.avgLatency, mutationDate: parseInt(elt.dateTime)})); 
+  const mutationLatencies = props.mutationData.map(elt => elt.avgLatency);
+  const mutationDates = props.mutationData.map(elt => parseInt(elt.dateTime));
   const subscribers = props.mutationData.map(elt => elt.expectedAqls);
-  const date = props.mutationData.map(elt => elt.dateTime); 
-  console.log(avgLatency);                                  
-  const mutationStartDate = new Date(date[0]);
-  console.log(mutationStartDate);
+  console.log(avgLatency);
+  console.log(min(mutationDates));
+  console.log(max(mutationDates));
 
   const svgRef = useRef();
   const wrapperRef = useRef();
@@ -39,8 +42,8 @@ function LineChart(props) {
 
     // scales + line generator
     const xScale = scaleLinear()
-      .domain([min(date), max(date)])
-      .range([10, width - 10]);
+      .domain([min(mutationDates), max(mutationDates)])
+      .range([0, width]);
 
     if (currentZoomState) {
       const newXScale = currentZoomState.rescaleX(xScale);
@@ -48,12 +51,12 @@ function LineChart(props) {
     }
 
     const yScale = scaleLinear()
-      .domain([0, max(avgLatency)])
-      .range([height - 10, 10]);
+      .domain([0, max(mutationLatencies) + 10])
+      .range([height, 0]);
 
     const lineGenerator = line()
-      .x((d) => xScale(d))
-      .y((d) => yScale(d))
+      .x((d) => xScale(d.mutationDate))
+      .y((d) => yScale(d.avgL))
       .curve(curveCardinal);
 
     // render the line
@@ -74,16 +77,23 @@ function LineChart(props) {
       .attr('stroke', 'lightblue')
       .attr('r', 2)
       .attr('fill', 'lightblue')
-      .attr('cx', (value, index) => xScale(index))
-      .attr('cy', yScale);
+      .attr('cx', (d) => xScale(d.mutationDate))
+      .attr('cy', (d) => yScale(d.avgL));
 
     // axes
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale)
+      .tickFormat(timeFormat('%Y-%m-%dT%H:%M:%S.%LZ'));
+
     svg
       .select('.x-axis')
       .attr('transform', `translate(0, ${height})`)
       .call(xAxis)
-      .style('color', 'white');
+      .style('color', 'white')
+      .selectAll('text')
+        .attr("transform", "translate(-10,10)rotate(-45)")
+        .style("text-anchor", "end")
+        // .style("font-size", 20)
+
 
     const yAxis = axisLeft(yScale);
     svg.select('.y-axis').style('color', 'white').call(yAxis);
