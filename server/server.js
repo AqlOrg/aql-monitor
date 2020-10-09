5556a94092859eddc8b6bf5226064ee139d734b6
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-09 12:15:09 -0700

    Add OAuth boilerplate.
    - Require passport and passport-github package
    
    - Build skeleton for OAuth, actual credentials are still needed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const GitHubStrategy = require('passport-github').Strategy;
-const passport = require('passport');
+// const GitHubStrategy = require('passport-github').Strategy;
+// const passport = require('passport');
 const app = express();
 const PORT = 3000;
 
@@ -18,34 +18,34 @@ app.use('/api', router);
 // ---------------------- GitHub OAuth Section ----------------------- //
 
 // Initialize Passport
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
 // Incorporating GitHub strategy with environment variables
-passport.use(new GitHubStrategy({
-  clientID: '', // process.env.GITHUB_CLIENT_ID
-  clientSecret: '', // process.env.GITHUB_CLIENT_SECRET
-  callbackURL: '', // callback URL from GitHub
-},
-  // Async callback function: params- tokens, GitHub profile, callback
-  async (accessToken, refreshToken, profile, cb) => {
-    console.log(profile);
-    // profile._json => profile information
-    cb(null, profile);
-  }
-));
+// passport.use(new GitHubStrategy({
+//   clientID: '', // process.env.GITHUB_CLIENT_ID
+//   clientSecret: '', // process.env.GITHUB_CLIENT_SECRET
+//   callbackURL: '', // callback URL from GitHub
+// },
+//   // Async callback function: params- tokens, GitHub profile, callback
+//   async (accessToken, refreshToken, profile, cb) => {
+//     console.log(profile);
+//     // profile._json => profile information
+//     cb(null, profile);
+//   }
+// ));
 
-// Setting up Express routing for POST request to login via OAuth
-app.get('/githublogin', passport.authenticate('github', { session: true }));
+// // Setting up Express routing for POST request to login via OAuth
+// app.get('/githublogin', passport.authenticate('github', { session: true }));
 
-// Callback with GitHub OAuth to eventually redirect users to dashboard
-app.get(
-  '/auth/github/callback',
-  passport.authenticate('github', { session: true }),
-  (req, res) => {
-    console.log(res);
-    res.sendStatus(418);
-  }
-);
+// // Callback with GitHub OAuth to eventually redirect users to dashboard
+// app.get(
+//   '/auth/github/callback',
+//   passport.authenticate('github', { session: true }),
+//   (req, res) => {
+//     console.log(res);
+//     res.sendStatus(418);
+//   }
+// );
 // ====================
 
 module.exports = app.listen(PORT, () => {
