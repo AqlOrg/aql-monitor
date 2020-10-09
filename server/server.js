c423c2433a992ebdbcd7b271326fee78f3f2d709
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-08 21:01:24 -0700

    Adding GitHub Passport OAuth Strategy to server.js file. This is the beginning of strategy incorporation.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const GitHubStrategy = require('passport-github').Strategy;
 const app = express();
 const PORT = 3000;
 
@@ -12,6 +13,8 @@ app.use(cors({ origin: ['http://localhost:8080', 'http://localhost:3000'] }));
 app.use(express.static('public'));
 
 app.use('/api', router);
+
+
 
 module.exports = app.listen(PORT, () => {
   console.log('Aql hears you loud and clear on port 3000');
