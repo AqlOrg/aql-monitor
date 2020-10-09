ee0f976dc5bd7c896d889483e7fb5cc5500cc0a9
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-08 21:27:17 -0700

    Refactoring Passport GitHub stratgegy for better visability with codebase

--- server/server.js
+++ server/server.js
@@ -15,21 +15,28 @@ app.use(express.static('public'));
 app.use('/api', router);
 
 // GitHub OAuth Section
+
+// Initialize Passport
 app.use(passport.initialize());
 
+// Incorporating GitHub strategy with environment variables
 passport.use(new GitHubStrategy({
-  clientID: '',
-  clientSecret: '',
-  callbackURL: '',
+  clientID: '', // process.env.GITHUB_CLIENT_ID
+  clientSecret: '', // process.env.GITHUB_CLIENT_SECRET
+  callbackURL: '', // callback URL from GitHub
 },
+  // Async callback function: params- tokens, GitHub profile, callback
   async (accessToken, refreshToken, profile, cb) => {
     console.log(profile);
+    // profile._json => profile information
     return profile;
   }
 ));
 
+// Setting up Express routing for POST request to login via OAuth
 app.get('/githublogin', passport.authenticate('github', { session: true }));
 
+// Callback with GitHub OAuth to eventually redirect users to dashboard
 app.get(
   '/auth/github/callback',
   passport.authenticate('github', { session: true }),
