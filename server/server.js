5ba24b5a5dc055ec32d89fb68fa8c4cde833fabf
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-11 20:17:25 -0700

    App signup to github OAuth is complete. Added clientID, clientSecret, amd callbackURL.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const GitHubStrategy = require('passport-github').Strategy;
-// const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require('passport');
 const app = express();
 const PORT = 3000;
 
@@ -18,35 +18,35 @@ app.use('/api', router);
 // ---------------------- GitHub OAuth Section ----------------------- //
 
 // Initialize Passport
-// app.use(passport.initialize());
+app.use(passport.initialize());
 
 // Incorporating GitHub strategy with environment variables
-// passport.use(new GitHubStrategy({
-//   clientID: '', // process.env.GITHUB_CLIENT_ID
-//   clientSecret: '', // process.env.GITHUB_CLIENT_SECRET
-//   callbackURL: '', // callback URL from GitHub
-// },
-//   // Async callback function: params- tokens, GitHub profile, callback
-//   async (accessToken, refreshToken, profile, cb) => {
-//     console.log(profile);
-//     // profile._json => profile information
-//     cb(null, profile);
-//   }
-// ));
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID, // process.env.GITHUB_CLIENT_ID
+  clientSecret: process.env.GITHUB_CLIENT_SECRET, // process.env.GITHUB_CLIENT_SECRET
+  callbackURL: 'http://localhost:8080/auth/github/callback', // callback URL from GitHub
+},
+  // Async callback function: params- tokens, GitHub profile, callback
+  async (accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+    // profile._json => profile information
+    cb(null, profile);
+  }
+));
 
-// // Setting up Express routing for POST request to login via OAuth
-// app.get('/githublogin', passport.authenticate('github', { session: true }));
+// Setting up Express routing for POST request to login via OAuth
+app.get('/githublogin', passport.authenticate('github', { session: false }));
 
-// // Callback with GitHub OAuth to eventually redirect users to dashboard
-// app.get(
-//   '/auth/github/callback',
-//   passport.authenticate('github', { session: true }),
-//   (req, res) => {
-//     console.log(res);
-//     res.sendStatus(418);
-//   }
-// );
-// ====================
+// Callback with GitHub OAuth to eventually redirect users to dashboard
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { session: false }),
+  (req, res) => {
+    console.log(res);
+    res.sendStatus(418);
+  }
+);
+//====================
 
 module.exports = app.listen(PORT, () => {
   console.log('Aql hears you loud and clear on port 3000');
