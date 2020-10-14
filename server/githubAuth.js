3cbb44513111d47c42cbcd0d2eee378c252dce7f
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-14 11:40:43 -0700

    Add other user identifiers such as display name and guthub id as string types to the database

--- server/githubAuth.js
+++ server/githubAuth.js
@@ -1,10 +1,9 @@
 export default (sequilize, DataTypes) => {
   //database table name 
   const githubAuth = sequilize.define('users', {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-    },
-    
+    username: DataTypes.STRING,
+    display_name: DataTypes.STRING,
+    github_ID: DataTypes.STRING,
+    avatar_url: DataTypes.STRING,
   })
 }
