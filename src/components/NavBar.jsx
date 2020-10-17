2e063c1f7681a5ba892ab10ca3da4385c0af658d
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-17 14:35:24 -0700

    Create a logout button that redirects user to landing page
    Co-authored-by: Julie Pinchak <jpinchak@gmail.com>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+
+function logout() {
+  console.log('this is happening');
+  Cookies.remove('userToken', { path: ''})
+  window.location.reload(false)
+}
+
 
 function NavBar() {
-  // const [userName, setUserName] = useState('Raql');
 
   return (
     <div id="navbar">
       <span id="logo">Aql</span>
-      <button onClick={() => clickbutton(userName)}>J</button>
+      <button id="userbutton" onClick={() => clickbutton(userName)}>J</button>
+      <button id="logoutbutton" value="/githublogin" onClick={() => logout()}>Logout</button>
     </div>
   );
 }
