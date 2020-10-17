f077be1917ff06743c84b7dc5c3ff995059b18dc
Rocio-Infante <shadesofroseblog@gmail.com>
2020-10-17 11:05:37 -0700

    Inserted our middleware for user data into Router in router.js file
    Co-authored-by: Julie Pinchak <jpinchak@gmail.com>

--- server/router.js
+++ server/router.js
@@ -3,8 +3,11 @@ const Router = express.Router();
 const aqlDatabaseController = require('./controllers/AqlDatabaseController');
 
 
-Router.get('/', aqlDatabaseController.getAqls, (req, res) => {
-  res.status(200).json(res.locals.data);
+Router.get('/', 
+  aqlDatabaseController.getAqls,
+  aqlDatabaseController.getUserData,
+  (req, res) => {
+    res.status(200).json(res.locals.data);
 });
 
 module.exports = Router;
