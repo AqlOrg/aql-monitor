c19a46a2e53b5563f8756909759b1400e0617666
jpinchak <jpinchak@gmail.com>
2020-10-19 22:41:04 -0700

    made extensive changes to styling of landing page

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, NavLink } from 'react-router-dom';
 import Navbar from './navbar.jsx';
+import aqlLogo from '../../../public/littleaql.png';
 
 function LandingPage() {
   return (
     <div>
       <Navbar/>
+      <div id="landingdiv">
+        <div id="welcome">Welcome to Aql</div>
+        <div id="aqlholder">
+          <img id="landingAql" src={aqlLogo} />
+        </div>
+        <div className="buttonholder">
+          <Router forceRefresh={true}>
+            <NavLink to="/githublogin">
+              <button id='loginbutton'>
+                Signup/Login
+              </button>
+            </NavLink>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Cookies from 'js-cookie';
 
 function logout() {
@@ -6,14 +6,42 @@ function logout() {
   window.location.reload(false)
 }
 
-
 function NavBar() {
+  const dropdownRef= useRef(null);
+  const [isActive, setIsActive] = useState(false);
+  const handleDropdown = () => setIsActive(!isActive);
+  
+  // collapse dropdown menu when clicking away
+  useEffect(() => {
+    const pageClickEvent = (e) => {
+      // If the active element exists and is clicked outside of
+      if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+        setIsActive(!isActive);
+      }
+    };
+
+    // if menu is active, then listen for clicks
+    if(isActive) {
+      window.addEventListener('click', pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener('click', pageClickEvent);
+    }
+
+  }, [isActive]);
 
   return (
     <div id="navbar">
       <span id="logo">Aql</span>
-      <button id="userbutton" onClick={() => clickbutton(userName)}>J</button>
-      <button id="logoutbutton" value="/githublogin" onClick={() => logout()}>Logout</button>
+      <button id="userbutton" onClick={handleDropdown} className="menu-trigger">J</button>
+      <nav ref={dropdownRef} className={`dropdown-menu ${isActive ? 'active' : 'inactive'}`}>
+        <ul>
+          <li> 
+            <button id="logoutbutton" value="/githublogin" onClick={() => logout()}>Logout</button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
