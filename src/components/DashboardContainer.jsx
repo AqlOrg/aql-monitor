6ab85815878d21e34802ea91dbe6d994fc03d0a7
jpinchak <jpinchak@gmail.com>
2020-10-20 15:43:09 -0700

    update dropdown button to include links to user token and readme

--- src/components/DashboardContainer.jsx
+++ src/components/DashboardContainer.jsx
@@ -3,24 +3,10 @@ import React, { useState, useEffect } from 'react';
 import TopRow from './TopRow/TopRow.jsx';
 import MiddleRow from './MiddleRow/MiddleRow.jsx';
 import BottomRow from './BottomRow/BottomRow.jsx';
-import Cookies from 'js-cookie';
-
-const userTokenCookie = Cookies.get('userToken');
 
 function DashboardContainer(props) {
-
   const [ready, setReady] = useState(false);
   const [aqlData, setAqlData] = useState({});
-  const [userToken, setUserToken] = useState(userTokenCookie);
-  const [userInfo, setUserInfo] = useState({}) 
-
-  // fetching user data
-  useEffect(() => {
-    fetch('/api/user')
-    // .then(res => res.json())
-    .then(res => setUserInfo(res))
-    .catch(err => console.log(err));
-  }, [userToken]);
   
   // fetching user analytics
   useEffect(() => {
